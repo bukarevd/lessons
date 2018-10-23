8cc40da47bacf72540507e2db3b6617c71717905
bukarevd <bukarevd@gmail.com>
2018-10-23 16:51:17 +0300

    Task04 (not ready)

--- src/Lesson3/Homework/Task04.java
+++ src/Lesson3/Homework/Task04.java
@@ -1,7 +1,21 @@
 package Lesson3.Homework;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Task04 {
     public static void main(String[] args) {
-        Min + (int)(Math.random() * ((Max - Min) + 1))
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        int[] intArr = new int[num];
+        int min = -5, max = 5;
+
+
+
+        for (int i = 0; i < intArr.length; i++){
+            intArr[i] = min + (int)(Math.random() * ((max - min) + 1));
+        }
+        System.out.println(Arrays.toString(intArr));
+//
     }
 }
