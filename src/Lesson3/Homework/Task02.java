f281180eb1b3373218a6890593aeff7e4278fc63
bukarevd <bukarevd@gmail.com>
2018-10-23 14:58:36 +0300

    task02

---
+++ src/Lesson3/Homework/Task02.java
@@ -0,0 +1,22 @@
+package Lesson3.Homework;
+
+import com.sun.istack.internal.NotNull;
+
+import java.util.Arrays;
+
+public class Task02 {
+    public static void main(String[] args) {
+        int[] intArr = new int[50];
+
+        for (int i =1, j = 0; i < 100; i += 2, j++){
+            intArr[j] = i;
+        }
+        for (int i = 0; i < intArr.length; i++)
+            System.out.print(intArr[i] + " ");
+
+        System.out.println();
+
+        for (int i = intArr.length - 1;  i >= 0; i--)
+            System.out.print(intArr[i] + " ");
+    }
+}
