a17780b8a0d8caf323da0808137bcb1786c00fc5
Denis <bukarevd@gmail.com>
2018-12-06 23:29:40 +0300

    homework

--- src/lesson17/homework/task01/CryptoInputStream.java
+++ src/lesson17/homework/task01/CryptoInputStream.java
@@ -1,4 +1,34 @@
 package lesson17.homework.task01;
 
-public class CryptoInputStream {
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class CryptoInputStream extends FilterInputStream {
+    private long readBytes;
+    byte[] pass;
+
+    public CryptoInputStream(InputStream in) {
+        super(in);
+    }
+
+    public CryptoInputStream(InputStream in, byte[] pass) {
+        super(in);
+        this.pass = pass;
+    }
+
+    @Override
+    public int read() throws IOException {
+        int res = super.read();
+        if (res >= 0) readBytes++;
+        return res;
+    }
+
+    @Override
+    public int read(byte[] b, int off, int len) throws IOException {
+        int res = super.read(b, off, len);
+        if (res >= 0) readBytes+=res;
+        res = (byte) (res ^ pass[0 % pass.length]);
+        return res;
+    }
 }
--- src/lesson17/homework/task01/CryptoOutputStream.java
+++ src/lesson17/homework/task01/CryptoOutputStream.java
@@ -1,6 +1,29 @@
 package lesson17.homework.task01;
 
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
-public class CryptoOutputStream {
+public class CryptoOutputStream extends FilterOutputStream {
+    private long readBytes;
+    byte[] pass;
+
+    @Override
+    public void write(int b) throws IOException {
+        int res = b;
+        if (res >= 0) readBytes+=res;
+        res = (byte) (res ^ pass[0 % pass.length]);
+        super.write(res);
+    }
+
+    public CryptoOutputStream(OutputStream out) {
+        super(out);
+    }
+
+    public CryptoOutputStream(OutputStream out, byte[] pass) {
+        super(out);
+        this.pass = pass;
+    }
 
 }
